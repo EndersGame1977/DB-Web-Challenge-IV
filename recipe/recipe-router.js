21eb82997e020d66fa4ac723bdf895ab54536fd9
Jason Prince <jasoncprince@gmail.com>
2019-09-12 15:04:09 -0600

    Created recipe-router.js

---
+++ recipe/recipe-router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const recipe = require("./scheme-model.js");
+
+const router = express.Router();
+
+// All recipes (without details about ingredients or steps)
+router.get("/", (req, res) => {});
+
+// A list of ingredients and quantites for a single recipe
+router.get("/:id/shoppingList", (req, res) => {});
+
+// A correctly ordered list of how to prepare a single recipe
+router.get("/:id/instructions", (req, res) => {});
+
+// All recipes in the system that utilize a single ingredient
+router.get("/:id/recipes", (req, res) => {});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const recipeRouter = require("./recipe/recipe-router.js");
 const server = express();
 
 server.use(express.json());
-server.use("/api/recipe", recipeRouter);
+server.use("/api/recipes", recipeRouter);
 
 module.exports = server;
